43b8171fa003df4485cc8a894bad437556c8815f
UP230635 <up230635@alumnos.upa.edu.mx>
2026-02-27 18:37:33 -0600

    Tarea completa

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus n =
+    if n >= 7 then
+        Approved
+
+    else if n < 0 then
+        Pending
+
+    else
+        Failed
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction accion =
+    case accion of
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction accion =
+    List.map airplaneScheduleAction accion
